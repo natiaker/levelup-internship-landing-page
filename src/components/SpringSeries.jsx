2a2fef4c8bb2cd4d7213efbd585851ef9f9a4012
natiaker <n.kerdzaia@gmail.com>
2023-04-24 19:54:01 +0400

    feat: add components to spring series tab

--- src/components/SpringSeries.jsx
+++ src/components/SpringSeries.jsx
@@ -1,10 +1,63 @@
 import React from "react";
 import TournamentsCard from "./TournamentsCard";
+import {
+  StyledMainCard,
+  StyledMainTitle,
+  StyledTopBorder,
+} from "../styles/ContentCardStyles";
+import LeaderboardCard from "./LeaderboardCard";
+import PrizesCard from "./PrizesCard";
+import { Typography, Link, Box } from "@mui/material";
+import SideLeaderboardCard from "./SideLeaderboardCard";
+import Rules from "./Rules";
+import Offers from "./Offers";
 
 const SpringSeries = () => {
   return (
     <>
       <TournamentsCard />
+      <StyledMainCard marginTop='27px'>
+        <StyledMainTitle marginBottom='20px'>
+          <StyledTopBorder />
+          ᲛᲝᲮᲕᲓᲘ TOP20 ᲚᲘᲓᲔᲠᲑᲝᲠᲓᲨᲘ ᲢᲣᲠᲜᲘᲠᲔᲑᲖᲔ ᲓᲐᲒᲠᲝᲕᲔᲑᲣᲚᲘ ᲥᲣᲚᲔᲑᲘᲗ
+        </StyledMainTitle>
+        {/* <ArrowDropDown /> */}
+        <Box
+          px={3}
+          pb={3}
+        >
+          <LeaderboardCard />
+          <PrizesCard />
+          <Typography
+            textAlign='center'
+            fontSize='14px'
+            marginBottom='12px'
+          >
+            * ლიდერბორდის შედეგები განახლდება{" "}
+            <Link
+              href='#'
+              color='background.active'
+            >
+              პოკერის ლობიში
+            </Link>
+          </Typography>
+          <Typography
+            fontSize='14px'
+            textAlign='center'
+          >
+            * სატურნირო ლიდერბორდის ქულების დათვლაში არ მონაწილეობს ფრიროლები და
+            სატელიტები
+          </Typography>
+        </Box>
+      </StyledMainCard>
+      {/* ყოველდღიური ტურნირენი და სატელიტები */}
+      <SideLeaderboardCard />
+      <Typography sx={{ marginBottom: "22px" }}>წესები და პირობები</Typography>
+      <Rules />
+      <Typography sx={{ marginTop: "55px", marginBottom: "32px" }}>
+        მსგავსი აქციები
+      </Typography>
+      <Offers />
     </>
   );
 };

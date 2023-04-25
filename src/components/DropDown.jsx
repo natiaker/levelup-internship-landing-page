fae7686d550009a087e61c346a809b5aa976ca5c
Natia Kerdzaia <silverlizard@Natias-MacBook-Air.local>
2023-04-25 14:22:01 +0400

    feat: add styles to dropdown

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -3,34 +3,61 @@ import { Select, MenuItem, FormControl, InputLabel, Box } from "@mui/material";
 
 const DropDown = () => {
   const [age, setAge] = useState("");
-  const handleChange = event => {
+  const handleChange = (event) => {
     setAge(event.target.value);
   };
   return (
     <Box sx={{ display: "flex", justifyContent: "center" }}>
       <FormControl
         sx={{
+          display: "flex",
+          justifyContent: "center",
           width: "320px",
           height: "30px",
-          bgcolor: "white",
+          marginBottom: "20px",
+          bgcolor: "background.secondary",
           borderRadius: "30px",
-
+          border: "2px solid #25292B",
           "& .MuiOutlinedInput-root": {
             "& fieldset": {
               height: "30px",
               border: "none",
             },
           },
+          "& .MuiInputLabel-root": {
+            color: "white",
+            top: "-12px",
+          },
+          svg: {
+            color: "white",
+            bgcolor: "#EF5A21",
+            border: "1px solid white",
+            borderRadius: "50%",
+            width: "35px",
+            height: "35px",
+            top: "9px",
+            right: "0",
+          },
         }}
       >
         <InputLabel>ქულების დაგროვების მექანიკა</InputLabel>
         <Select
           value={age}
+          label={"ქულები"}
           onChange={handleChange}
+          MenuProps={{
+            PaperProps: {
+              sx: {
+                color: "text.primary",
+                bgcolor: "background.default",
+                fontSize: 13,
+              },
+            },
+          }}
         >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
+          <MenuItem value={10}>Lorem ipsum dolor 1</MenuItem>
+          <MenuItem value={20}>Lorem ipsum dolor 2</MenuItem>
+          <MenuItem value={30}>Lorem ipsum dolor 3</MenuItem>
         </Select>
       </FormControl>
     </Box>
